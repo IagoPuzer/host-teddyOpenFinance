003d1b965f95c569d24a62272a82df1954e7c38b
Iago Puzer <iagopuzer06@hotmail.com>
2025-09-07 19:44:37 -0300

    refactor: remover componentes Header e Layout, simplificando a estrutura do aplicativo e atualizando a navegação para o componente CustomersApp

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -2,20 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 import AuthPage from "reactAppAuth/AuthPage";
 import CustomersApp from "reactAppCustomers/CustomersApp";
-import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
-      <Route
-        path="/customers"
-        element={
-          <Layout>
-            <CustomersApp />
-          </Layout>
-        }
-      />
+      <Route path="/customers" element={<CustomersApp />} />
     </Routes>
   );
 }
